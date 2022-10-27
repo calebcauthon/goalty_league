b8020bf5072ccbdff558d2bc16ef1659641ad825
caleb <calebcauthon@gmail.com>
2022-10-27 09:35:34 -0500

    Add goalty schedule builder

---
+++ captains.js
@@ -0,0 +1,47 @@
+var ROSTER = [
+  {
+    "Timestamp": "--",
+    "Name": "Monty",
+    "Email": "",
+    "Identifying Gender": "Male",
+    "Baggage": "",
+    "Attendance": "ALL",
+    "Experience": "Club"
+  },
+{
+    "Timestamp": "--",
+    "Name": "Rooster",
+    "Email": "",
+    "Identifying Gender": "Male",
+    "Baggage": "",
+    "Attendance": "ALL",
+    "Experience": "Club"
+  },
+  {
+    "Timestamp": "--",
+    "Name": "Mr Taylor",
+    "Email": "",
+    "Identifying Gender": "Male",
+    "Baggage": "",
+    "Attendance": "ALL",
+    "Experience": "Club"
+  },
+  {
+    "Timestamp": "--",
+    "Name": "Joe",
+    "Email": "",
+    "Identifying Gender": "Male",
+    "Baggage": "",
+    "Attendance": "ALL",
+    "Experience": "Club"
+  },
+  {
+    "Timestamp": "--",
+    "Name": "Beers",
+    "Email": "",
+    "Identifying Gender": "Male",
+    "Baggage": "",
+    "Attendance": "ALL",
+    "Experience": "Club"
+  }
+];
---
+++ roster2022.js
@@ -0,0 +1,452 @@
+var ROSTER = [
+//  {
+//    "Timestamp": "10/5/2022 15:22:49",
+//    "Name": "TR",
+//    "Email": "timbrobbins@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "Club"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 15:23:29",
+//    "Name": "Wheels",
+//    "Email": "Mjwheelock77@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "",
+//    "Attendance": "No Tourney",
+//    "Experience": "Summer League"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 15:24:26",
+//    "Name": "Brock",
+//    "Email": "Bahicks99@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "Jonah",
+//    "Attendance": "ALL",
+//    "Experience": "Club"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 15:26:59",
+//    "Name": "Stephanie Rupp",
+//    "Email": "stephrupp@gmail.com",
+//    "Identifying Gender": "Female",
+//    "Baggage": "CAITLYN",
+//    "Attendance": "ALL",
+//    "Experience": ""
+//  },
+//  {
+//    "Timestamp": "10/5/2022 15:27:50",
+//    "Name": "Bruce Campbell",
+//    "Email": "brucecampbell3@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "Club, College, Summer League"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 15:29:19",
+//    "Name": "Luke Grieger ",
+//    "Email": "lgrieger13@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "Spencer Roberts ",
+//    "Attendance": "ALL",
+//    "Experience": "Club, College, Summer League"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 15:33:21",
+//    "Name": "Cory Green",
+//    "Email": "Green.1065@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "Club, College, Summer League"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 15:35:11",
+//    "Name": "mei",
+//    "Email": "meibranit@gmail.com",
+//    "Identifying Gender": "Female",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "Club"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 15:41:46",
+//    "Name": "Courtney Walbe ",
+//    "Email": "cwalbe@gmail.com",
+//    "Identifying Gender": "Female",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "Club, College, Summer League"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 15:43:09",
+//    "Name": "Matt Beers",
+//    "Email": "beersmatthew2324@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "Club, College, Summer League"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 15:43:23",
+//    "Name": "Latrice Fletcher ",
+//    "Email": "latrice.england@gmail.com",
+//    "Identifying Gender": "Female",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "Summer League"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 16:02:07",
+//    "Name": "Ryan Tuschhoff",
+//    "Email": "ryantuschhoff@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "Steph Rupp",
+//    "Attendance": "ALL",
+//    "Experience": ""
+//  },
+//  {
+//    "Timestamp": "10/5/2022 16:06:26",
+//    "Name": "Jenna Galletta ",
+//    "Email": "jgalletta19@gmail.com",
+//    "Identifying Gender": "Female",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "Club, College, Summer League"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 17:02:02",
+//    "Name": "Michael Montesano ",
+//    "Email": "montym17@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "Club"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 17:25:15",
+//    "Name": "Sam Ward",
+//    "Email": "samgwhward@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "Sowmya ",
+//    "Attendance": "ALL",
+//    "Experience": "Club, College"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 17:32:17",
+//    "Name": "Sowmya",
+//    "Email": "Ragothaman98@gmail.com",
+//    "Identifying Gender": "Female",
+//    "Baggage": "Sam Ward ",
+//    "Attendance": "ALL",
+//    "Experience": "Club, College, Summer League"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 19:01:26",
+//    "Name": "Clare Frantz",
+//    "Email": "clare.frantz12@gmail.com",
+//    "Identifying Gender": "Female",
+//    "Baggage": "Matty Marcus",
+//    "Attendance": "ALL",
+//    "Experience": "Club, College, Summer League"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 19:05:23",
+//    "Name": "Janani Ragothaman ",
+//    "Email": "jsragothaman@gmail.com",
+//    "Identifying Gender": "Female",
+//    "Baggage": "",
+//    "Attendance": "Missing Nov 8",
+//    "Experience": "Club, College, Summer League"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 19:56:26",
+//    "Name": "Jonah Larsen",
+//    "Email": "j3sports@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "Brock Hicks",
+//    "Attendance": "ALL",
+//    "Experience": "Club, College, Summer League"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 20:02:18",
+//    "Name": "Lauren Wiggins",
+//    "Email": "wigg1997@gmail.com",
+//    "Identifying Gender": "Female",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "Club, College, Summer League"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 20:07:04",
+//    "Name": "Amanda Borders",
+//    "Email": "amandaborders91@gmail.com",
+//    "Identifying Gender": "Female",
+//    "Baggage": "",
+//    "Attendance": "Missing First Night",
+//    "Experience": "Club"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 20:12:15",
+//    "Name": "Mr. Taylor Hession",
+//    "Email": "taylorhession17@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "no",
+//    "Attendance": "ALL",
+//    "Experience": "Club, College, Summer League"
+//  },
+  {
+    "Timestamp": "10/5/2022 20:12:52",
+    "Name": "Christian simpson ",
+    "Email": "Cwsimpson987@gmail.com",
+    "Identifying Gender": "Male",
+    "Baggage": "",
+    "Attendance": "ALL",
+    "Experience": "Club"
+  },
+  {
+    "Timestamp": "",
+    "Name": "Caitlyn Lisk",
+    "Email": "",
+    "Identifying Gender": "Female",
+    "Baggage": "",
+    "Attendance": "ALL",
+    "Experience": "Club"
+  },
+//  {
+//    "Timestamp": "10/5/2022 20:22:08",
+//    "Name": "Joe Young ",
+//    "Email": "ijustwantmymail@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "Club"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 20:25:24",
+//    "Name": "Dustin Wolfe",
+//    "Email": "dustintaylorwolfe@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "Club, College"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 20:36:35",
+//    "Name": "Branden Schneider",
+//    "Email": "Brandens71@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "Club"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 20:41:54",
+//    "Name": "Spencer Roberts",
+//    "Email": "Spence1529@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "Luke Grieger",
+//    "Attendance": "ALL",
+//    "Experience": "Club"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 21:29:45",
+//    "Name": "The Maaaaaacus",
+//    "Email": "matthew.marcus@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "Claire Hidaka",
+//    "Attendance": "ALL",
+//    "Experience": "Club, College, Summer League"
+//  },
+//  {
+//    "Timestamp": "10/5/2022 21:48:14",
+//    "Name": "jess tiller",
+//    "Email": "jtiller25@gmail.com",
+//    "Identifying Gender": "Female",
+//    "Baggage": "Taylor Hession",
+//    "Attendance": "ALL",
+//    "Experience": "Club, College, Summer League"
+//  },
+//  {
+//    "Timestamp": "10/6/2022 7:09:24",
+//    "Name": "Pj",
+//    "Email": "Peter.matulka@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "Club"
+//  },
+//  {
+//    "Timestamp": "10/6/2022 8:46:37",
+//    "Name": "Christina Baker",
+//    "Email": "cbaker9010@gmail.com",
+//    "Identifying Gender": "Female",
+//    "Baggage": "Wheels ",
+//    "Attendance": "ALL",
+//    "Experience": "Club"
+//  },
+//  {
+//    "Timestamp": "10/6/2022 17:13:01",
+//    "Name": "Allen Jones ",
+//    "Email": "allenjones.mo@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "",
+//    "Attendance": "Missing First Night",
+//    "Experience": "Club"
+//  },
+//  {
+//    "Timestamp": "10/6/2022 22:01:58",
+//    "Name": "Ethan Knobloch",
+//    "Email": "runethanrun@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "Club, Summer League"
+//  },
+//  {
+//    "Timestamp": "10/7/2022 9:07:29",
+//    "Name": "Holly Webb",
+//    "Email": "webb.hollyr@gmail.com",
+//    "Identifying Gender": "Female",
+//    "Baggage": "Abby Acker",
+//    "Attendance": "ALL",
+//    "Experience": "Club"
+//  },
+//  {
+//    "Timestamp": "10/7/2022 13:28:12",
+//    "Name": "Aidan Moody",
+//    "Email": "Celticpole@hotmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "Club"
+//  },
+//  {
+//    "Timestamp": "10/7/2022 14:13:15",
+//    "Name": "Abby Acker",
+//    "Email": "aacker11@gmail.com",
+//    "Identifying Gender": "Female",
+//    "Baggage": "Holly Webb",
+//    "Attendance": "ALL",
+//    "Experience": "Club, College, Summer League"
+//  },
+//  {
+//    "Timestamp": "10/7/2022 14:17:35",
+//    "Name": "Austin Heineman",
+//    "Email": "adh6325@truman.edu",
+//    "Identifying Gender": "Male",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "Club, College, Summer League"
+//  },
+//  {
+//    "Timestamp": "10/8/2022 16:44:08",
+//    "Name": "Aaron Johnson ",
+//    "Email": "aaron.johnson173@yahoo.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "",
+//    "Attendance": "Missing First Night",
+//    "Experience": "Club, College, Summer League"
+//  },
+//  {
+//    "Timestamp": "10/8/2022 19:20:22",
+//    "Name": "Brian Ebbitts",
+//    "Email": "bebbitts@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "Summer League"
+//  },
+//  {
+//    "Timestamp": "10/8/2022 19:21:50",
+//    "Name": "Brian Ebbitts",
+//    "Email": "bebbitts@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "Summer League"
+//  },
+//  {
+//    "Timestamp": "10/9/2022 12:22:10",
+//    "Name": "Amanda Johnson",
+//    "Email": "Aj10788@gmail.com",
+//    "Identifying Gender": "Female",
+//    "Baggage": "None",
+//    "Attendance": "ALL",
+//    "Experience": "Club"
+//  },
+//  {
+//    "Timestamp": "10/9/2022 17:33:02",
+//    "Name": "Taylor Cauthon",
+//    "Email": "tdsimpson90@gmail.com",
+//    "Identifying Gender": "Female",
+//    "Baggage": "Caleb Cauthon",
+//    "Attendance": "ALL",
+//    "Experience": "Club, Summer League"
+//  },
+//  {
+//    "Timestamp": "10/10/2022 10:13:31",
+//    "Name": "Caleb Cauthon",
+//    "Email": "CalebCauthon@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "Taylor Cauthon",
+//    "Attendance": "ALL",
+//    "Experience": "Club"
+//  },
+//  {
+//    "Timestamp": "10/11/2022 11:22:28",
+//    "Name": "Benton habel",
+//    "Email": "bentonhabel@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "Club, College, Summer League"
+//  },
+//  {
+//    "Timestamp": "10/11/2022 15:04:40",
+//    "Name": "Creighton Miller",
+//    "Email": "creighton.chemistry+goalty@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "Club"
+//  },
+//  {
+//    "Timestamp": "10/11/2022 15:29:56",
+//    "Name": "Christopher Melton ",
+//    "Email": "christophermelton.89@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "Creighton Miller",
+//    "Attendance": "ALL",
+//    "Experience": "Summer League"
+//  },
+//  {
+//    "Timestamp": "10/11/2022 20:54:38",
+//    "Name": "KJ Bryant",
+//    "Email": "kylejlbryant@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "",
+//    "Attendance": "ALL",
+//    "Experience": "College, Summer League"
+//  },
+//  {
+//    "Timestamp": "10/12/2022 10:29:52",
+//    "Name": "Rebekah Seyfert",
+//    "Email": "rseyfert1@gmail.com",
+//    "Identifying Gender": "Female",
+//    "Baggage": "Bryce Badgley",
+//    "Attendance": "ALL",
+//    "Experience": "Club, College, Summer League"
+//  },
+//  {
+//    "Timestamp": "10/12/2022 10:36:56",
+//    "Name": "Bryce Badgley",
+//    "Email": "bryce.badgley@gmail.com",
+//    "Identifying Gender": "Male",
+//    "Baggage": "Bekah Seyfert",
+//    "Attendance": "ALL",
+//    "Experience": "Summer League"
+//  }
+]
